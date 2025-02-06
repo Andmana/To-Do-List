4b959ca4501d70cc3b00a8402dfa37b187209d2a
andmana <iam.andmana@gmail.com>
2025-02-06 20:15:20 +0700

    Add pending counter updater on sidebar

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 import "./assets/css/styles.css";
 import { generateIcon } from "./module/imageSauce";
+import { updatePendingCount } from "./module/domManipulation";
+import { generateProjectNavs } from "./module/domInitialize";
+import { Task } from "./class/Task";
+import { Project } from "./class/Project";
+import { countAllTasksBy } from "./module/queryLocalStorage";
+import { addDays } from "date-fns";
 console.log("Hi Webpack");
+
+let today = new Date();
+const tasks = [
+    new Task("Task 1", "Description 1", today, "Low", "Default", false),
+    new Task("Task 1", "Description 1", today, "Medium", "Default", false),
+    new Task("Task 2", "Description 2", today, "High", "Autonomous", false),
+    new Task(
+        "Task 3",
+        "Description 3",
+        addDays(today, 1),
+        "Low",
+        "Default",
+        false
+    ),
+    new Task(
+        "Task 4",
+        "Description 4",
+        addDays(today, 3),
+        "High",
+        "Autonomous",
+        false
+    ),
+];
+
+const projects = [new Project("Default"), new Project("Autonomous")];
+
+window.count = (is, by) => {
+    console.log(countAllTasksBy(is, by));
+};
---
+++ src/module/domManipulation.js
@@ -0,0 +1,10 @@
+import { countAllTasksBy } from "./queryLocalStorage";
+
+export const updatePendingCount = (() => {
+    const counters = document.querySelectorAll("#tasks-nav .pending-count");
+    const filters = ["today", "tomorrow", "week", null];
+
+    counters.forEach((count, index) => {
+        count.innerHTML = countAllTasksBy(false, filters[index]);
+    });
+})();
