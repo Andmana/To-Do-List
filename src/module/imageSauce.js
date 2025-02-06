f8d2bb4a966ae19a96708b862c1ba351b7febbc9
andmana <iam.andmana@gmail.com>
2025-02-06 19:30:52 +0700

    Init image sourcing

---
+++ src/module/imageSauce.js
@@ -0,0 +1,26 @@
+import allIcon from "../assets/icons/all.svg";
+import completedIcon from "../assets/icons/completed.svg";
+import logoIcon from "../assets/icons/logo.svg";
+import addIcon from "../assets/icons/add.svg";
+import todayIcon from "../assets/icons/today.svg";
+import tomorrowIcon from "../assets/icons/tomorrow.svg";
+import weekIcon from "../assets/icons/week.svg";
+import sidebarIcon from "../assets/icons/sidebar.svg";
+
+export const generateStaticIcon = (() => {
+    const taskIcons = [
+        todayIcon,
+        tomorrowIcon,
+        weekIcon,
+        allIcon,
+        completedIcon,
+    ];
+    const taskImgs = document.querySelectorAll(".task-link > img");
+    taskImgs.forEach((link, index) => {
+        link.src = taskIcons[index];
+    });
+
+    document.querySelector("#sidebar-logo > img").src = sidebarIcon;
+    document.querySelector("#header > img").src = logoIcon;
+    document.querySelector("#add-task > img").src = addIcon;
+})();
