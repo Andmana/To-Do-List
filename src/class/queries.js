17a1c9fa0b84bb4313ef6f33abcc3ab0c5bcf616
andmana <iam.andmana@gmail.com>
2025-02-07 22:00:46 +0700

    Update data storage management and the queries

--- src/class/queries.js
+++ src/class/queries.js
@@ -2,31 +2,39 @@ import { Project } from "./Project";
 import { isThisWeek, isToday, isTomorrow } from "date-fns";
 import { Task } from "./Task";
 
-const getFromLocalStorage = (key) =>
-    JSON.parse(localStorage.getItem(key)) || [];
+export const getFromLocalStorage = () => {
+    const data = localStorage.getItem("myToDoList");
+    return data ? JSON.parse(data) : {}; // Gracefully return empty object if not found
+};
+
+export const saveToLocalStorage = (projects, tasks) => {
+    localStorage.setItem("myToDoList", JSON.stringify({ projects, tasks }));
+};
+
+export const updateLocalStorage = (projects = undefined, tasks = undefined) => {
+    const data = getFromLocalStorage();
+    if (projects !== undefined) data.projects = projects;
+    if (tasks !== undefined) data.tasks = tasks;
+    localStorage.setItem("myToDoList", JSON.stringify(data));
+};
+
+export function getAllProjects() {
+    const { projects = [] } = getFromLocalStorage(); // Default to empty array if no projects
+    return mapProjects(projects);
+}
+
+export function getAllTasks() {
+    const { tasks = [] } = getFromLocalStorage(); // Default to empty array if no tasks
+    return mapTasks(tasks);
+}
 
 const mapProjects = (projects) => projects.map(({ name }) => new Project(name));
 
 const mapTasks = (tasks) =>
     tasks.map(
         ({ title, description, dueDate, priority, project, isCompleted }) =>
-            new Task(
-                title,
-                description,
-                dueDate,
-                priority,
-                project,
-                isCompleted
-            )
+            new Task(title, description, dueDate, priority, project, isCompleted)
     );
-
-export function getAllProjects() {
-    return mapProjects(getFromLocalStorage("projects").projects || []);
-}
-
-export function getAllTasks() {
-    return mapTasks(getFromLocalStorage("tasks").tasks || []);
-}
 
 export function getAllTasksBy(isCompleted, due, project) {
     const filters = {
@@ -37,10 +45,8 @@ export function getAllTasksBy(isCompleted, due, project) {
 
     let tasks = getAllTasks();
 
-    if (filters[due])
-        tasks = tasks.filter((task) => filters[due](task.dueDate));
-    if (isCompleted != null)
-        tasks = tasks.filter((task) => task.isCompleted === isCompleted);
+    if (filters[due]) tasks = tasks.filter((task) => filters[due](task.dueDate));
+    if (isCompleted != null) tasks = tasks.filter((task) => task.isCompleted === isCompleted);
     if (project) tasks = tasks.filter((task) => task.project === project);
 
     return tasks;
@@ -49,14 +55,10 @@ export function getAllTasksBy(isCompleted, due, project) {
 export const countAllTasksBy = (isCompleted, due, project) =>
     getAllTasksBy(isCompleted, due, project).length;
 
-export function updateProjectsData(projects) {
-    localStorage.setItem("projects", JSON.stringify({ projects }));
-}
-
-export function updateTasksData(tasks) {
-    tasks.sort((a, b) => new Date(a.dueDate) - new Date(b.dueDate));
-    localStorage.setItem("tasks", JSON.stringify({ tasks }));
-}
+// export function updateTasksData(tasks) {
+//     tasks.sort((a, b) => new Date(a.dueDate) - new Date(b.dueDate));
+//     localStorage.setItem("tasks", JSON.stringify({ tasks }));
+// }
 
 export function saveProject(name, id = null) {
     let projects = getAllProjects();
@@ -67,15 +69,13 @@ export function saveProject(name, id = null) {
             updateProjectsData(projects);
         }
     } else {
-        if (
-            projects.some((pj) => pj.name.toLowerCase() === name.toLowerCase())
-        ) {
+        if (projects.some((pj) => pj.name.toLowerCase() === name.toLowerCase())) {
             return;
         }
 
         // Create a new project and add it to the list
         const project = new Project(name);
         projects.push(project);
-        updateProjectsData(projects);
+        updateLocalStorage(projects);
     }
 }
--- src/index.js
+++ src/index.js
@@ -4,23 +4,17 @@ import { Task } from "./class/Task";
 import { Project } from "./class/Project";
 import { addDays } from "date-fns";
 
-import { getAllProjects, getAllTasks, updateProjectsData, updateTasksData } from "./class/queries";
+//Run JS
+import { init } from "./module/app";
 
-let today = new Date();
-let projects = getAllProjects();
-let tasks = getAllTasks();
-if (!projects) {
-    tasks = [
-        new Task("Task 0", "Description 1", today, "Low", "Default", false),
-        new Task("Task 1", "Description 1", undefined, "Medium", "Default", true),
-        new Task("Task 2", "Description 2", addDays(today, 1), "High", "Default", false),
-        new Task("Task 3", "Description 3", addDays(today, 8), "Low", "Default", true),
-    ];
+import { getAllTasks, getFromLocalStorage, saveToLocalStorage } from "./class/queries";
 
-    projects = [new Project("Default")];
+let tasks = [new Task("Task 0", "Description 1", today, "Low", "Default", true)];
 
-    updateProjectsData(projects);
-    updateTasksData(tasks);
+projects = [new Project("Default")];
+let data = getFromLocalStorage();
+if (Object.keys(data).length === 0) {
+    saveToLocalStorage(projects, tasks);
 }
 
 window.retrive = (is, by, group) => {
@@ -30,8 +24,3 @@ window.retrive = (is, by, group) => {
 window.get = () => {
     console.log(getAllTasks());
 };
-
-import { init } from "./module/app";
-
-import { printState } from "./module/state";
-window.state = () => printState();
