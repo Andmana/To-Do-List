9d36f579b930a3fc4a6d7f06d2e437c76efb51ed
andmana <iam.andmana@gmail.com>
2025-02-10 22:17:42 +0700

    Add pointer and change icons

--- src/module/imageSauce.js
+++ src/module/imageSauce.js
@@ -5,7 +5,8 @@ import addIcon from "../assets/icons/add.svg";
 import todayIcon from "../assets/icons/today.svg";
 import tomorrowIcon from "../assets/icons/tomorrow.svg";
 import weekIcon from "../assets/icons/week.svg";
-import sidebarIcon from "../assets/icons/sidebar.svg";
+import sidebarIcon from "../assets/icons/sidebar-1.svg";
+import sidebar2Icon from "../assets/icons/sidebar-2.svg";
 
 export const loadStaticIcon = () => {
     const taskIcons = [todayIcon, tomorrowIcon, weekIcon, allIcon, completedIcon];
@@ -15,7 +16,7 @@ export const loadStaticIcon = () => {
     });
 
     document.querySelector("#sidebar-icon-1st > img").src = sidebarIcon;
-    document.querySelector("#sidebar-icon-2nd > img").src = sidebarIcon;
+    document.querySelector("#sidebar-icon-2nd > img").src = sidebar2Icon;
     document.querySelector("#main-logo > img").src = logoIcon;
     document.querySelector("#add-task > img").src = addIcon;
     document.querySelector("#add-project > img").src = addIcon;
